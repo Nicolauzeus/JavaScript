e7e9f15d10d1fc9f50b29247d3d8aebd7f40508c
Nicolauzeus <capingananicolau@gmail.com>
2026-03-27 21:21:12 -0300

    import js in html

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log
+//tipos primitivos
+
+//string
+let nome = "Zeus";
+console.log(nome);
+
+//number
+let idade = 22;
+console.log(idade);
+
+//boolean
+let isStudent = true;
+console.log(isStudent);
+
+//undefined
+let endereco;
+console.log(endereco);
+
+//null
+let telefone = null;
+console.log(telefone);
+
+//symbol
+let id = Symbol("id");
+console.log(id);
+
+//bigint
+let bigNumber = 123456789012345678901234567890n;
+console.log(bigNumber); 
